b15df4cf65db0160bc15bfc029ae31ae438583b8
Vincent <papelardvincent@gmail.com>
2024-07-28 11:55:54 +0200

    :construction::alembic:Currently figuring out how to receive, process and listen to FM audio data

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import asyncio
 
+import numpy as np
+import scipy
 from matplotlib.pyplot import *
 from matplotlib import animation
 from rtlsdr import RtlSdr
@@ -35,19 +37,209 @@ def live_plot():
     finally:
         sdr.close()
 
-def play_audio_sample():
+def listen_fm():
     """
-    Plays a short audio sample from the radio station being received.
+    Receives waves from the specified frequency, demodulates them
+    as FM and saves it in the current folder.
     """
-    from scipy.io.wavfile import write
-    import numpy as np
 
-    #samples = sdr.read_samples(256*1024)
-    plot(samples)
-    """rate = 44100
-    #scaled = np.int16(samples / np.max(np.abs(samples)) * 32767)
-    write('test.wav', rate, samples)"""
+    x = np.fromfile('./samples.iq', dtype=np.complex64)
+    # Quadrature Demod
+    x = 0.5 * np.angle(x[0:-1] * np.conj(x[1:])) # see https://wiki.gnuradio.org/index.php/Quadrature_Demod
+    # Freq shift
+    N = len(x)
+    f_o = -57e3 # amount we need to shift by
+    t = np.arange(N)/sdr.sample_rate # time vector
+    x = x * np.exp(2j*np.pi*f_o*t) # down shift
+    # Low-Pass Filter
+    taps = scipy.signal.firwin(numtaps=101, cutoff=7.5e3, fs=sdr.sample_rate)
+    x = np.convolve(x, taps, 'valid')
 
+    # Decimate by 10, now that we filtered and there wont be aliasing
+    x = x[::10]
+    sample_rate = 25e3
+
+    # Resample to 19kHz
+    x = scipy.signal.resample_poly(x, 19, 25) # up, down
+    sample_rate = 19e3
+
+    # Symbol sync, using what we did in sync chapter
+    samples = x # for the sake of matching the sync chapter
+    samples_interpolated = scipy.signal.resample_poly(samples, 32, 1) # we'll use 32 as the interpolation factor, arbitrarily chosen, seems to work better than 16
+    sps = 16
+    mu = 0.01 # initial estimate of phase of sample
+    out = np.zeros(len(samples) + 10, dtype=np.complex64)
+    out_rail = np.zeros(len(samples) + 10, dtype=np.complex64) # stores values, each iteration we need the previous 2 values plus current value
+    i_in = 0 # input samples index
+    i_out = 2 # output index (let first two outputs be 0)
+    while i_out < len(samples) and i_in+32 < len(samples):
+        out[i_out] = samples_interpolated[i_in*32 + int(mu*32)] # grab what we think is the "best" sample
+        out_rail[i_out] = int(np.real(out[i_out]) > 0) + 1j*int(np.imag(out[i_out]) > 0)
+        x = (out_rail[i_out] - out_rail[i_out-2]) * np.conj(out[i_out-1])
+        y = (out[i_out] - out[i_out-2]) * np.conj(out_rail[i_out-1])
+        mm_val = np.real(y - x)
+        mu += sps + 0.01*mm_val
+        i_in += int(np.floor(mu)) # round down to nearest int since we are using it as an index
+        mu = mu - np.floor(mu) # remove the integer part of mu
+        i_out += 1 # increment output index
+    x = out[2:i_out] # remove the first two, and anything after i_out (that was never filled out)
+
+    # Fine freq sync
+    samples = x # for the sake of matching the sync chapter
+    N = len(samples)
+    phase = 0
+    freq = 0
+    # These next two params is what to adjust, to make the feedback loop faster or slower (which impacts stability)
+    alpha = 8.0
+    beta = 0.02
+    out = np.zeros(N, dtype=np.complex64)
+    freq_log = []
+    for i in range(N):
+        out[i] = samples[i] * np.exp(-1j*phase) # adjust the input sample by the inverse of the estimated phase offset
+        error = np.real(out[i]) * np.imag(out[i]) # This is the error formula for 2nd order Costas Loop (e.g. for BPSK)
+
+        # Advance the loop (recalc phase and freq offset)
+        freq += (beta * error)
+        freq_log.append(freq * sample_rate / (2*np.pi)) # convert from angular velocity to Hz for logging
+        phase += freq + (alpha * error)
+
+        # Optional: Adjust phase so its always between 0 and 2pi, recall that phase wraps around every 2pi
+        while phase >= 2*np.pi:
+            phase -= 2*np.pi
+        while phase < 0:
+            phase += 2*np.pi
+    x = out
+
+    # Demod BPSK
+    bits = (np.real(x) > 0).astype(int) # 1's and 0's
+
+    # Differential decoding, so that it doesn't matter whether our BPSK was 180 degrees rotated without us realizing it
+    bits = (bits[1:] - bits[0:-1]) % 2
+    bits = bits.astype(np.uint8) # for decoder
+
+
+    # Constants
+    syndrome = [383, 14, 303, 663, 748]
+    offset_pos = [0, 1, 2, 3, 2]
+    offset_word = [252, 408, 360, 436, 848]
+
+    # see Annex B, page 64 of the standard
+    def calc_syndrome(x, mlen):
+        reg = 0
+        plen = 10
+        for ii in range(mlen, 0, -1):
+            reg = (reg << 1) | ((x >> (ii-1)) & 0x01)
+            if (reg & (1 << plen)):
+                reg = reg ^ 0x5B9
+        for ii in range(plen, 0, -1):
+            reg = reg << 1
+            if (reg & (1 << plen)):
+                reg = reg ^ 0x5B9
+        return reg & ((1 << plen) - 1) # select the bottom plen bits of reg
+
+    # Initialize all the working vars we'll need during the loop
+    synced = False
+    presync = False
+
+    wrong_blocks_counter = 0
+    blocks_counter = 0
+    group_good_blocks_counter = 0
+
+    reg = np.uint32(0) # was unsigned long in C++ (64 bits) but numpy doesn't support bitwise ops of uint64, I don't think it gets that high anyway
+    lastseen_offset_counter = 0
+    lastseen_offset = 0
+
+    # the synchronization process is described in Annex C, page 66 of the standard */
+    bytes_out = []
+    for i in range(len(bits)):
+        # in C++ reg doesn't get init so it will be random at first, for ours its 0s
+        # It was also an unsigned long but never seemed to get anywhere near the max value
+        # bits are either 0 or 1
+        reg = np.bitwise_or(np.left_shift(reg, 1), bits[i]) # reg contains the last 26 rds bits. these are both bitwise ops
+        if not synced:
+            reg_syndrome = calc_syndrome(reg, 26)
+            for j in range(5):
+                if reg_syndrome == syndrome[j]:
+                    if not presync:
+                        lastseen_offset = j
+                        lastseen_offset_counter = i
+                        presync = True
+                    else:
+                        if offset_pos[lastseen_offset] >= offset_pos[j]:
+                            block_distance = offset_pos[j] + 4 - offset_pos[lastseen_offset]
+                        else:
+                            block_distance = offset_pos[j] - offset_pos[lastseen_offset]
+                        if (block_distance*26) != (i - lastseen_offset_counter):
+                            presync = False
+                        else:
+                            print('Sync State Detected')
+                            wrong_blocks_counter = 0
+                            blocks_counter = 0
+                            block_bit_counter = 0
+                            block_number = (j + 1) % 4
+                            group_assembly_started = False
+                            synced = True
+                break # syndrome found, no more cycles
+
+        else: # SYNCED
+            # wait until 26 bits enter the buffer */
+            if block_bit_counter < 25:
+                block_bit_counter += 1
+            else:
+                good_block = False
+                dataword = (reg >> 10) & 0xffff
+                block_calculated_crc = calc_syndrome(dataword, 16)
+                checkword = reg & 0x3ff
+                if block_number == 2: # manage special case of C or C' offset word
+                    block_received_crc = checkword ^ offset_word[block_number]
+                    if (block_received_crc == block_calculated_crc):
+                        good_block = True
+                    else:
+                        block_received_crc = checkword ^ offset_word[4]
+                        if (block_received_crc == block_calculated_crc):
+                            good_block = True
+                        else:
+                            wrong_blocks_counter += 1
+                            good_block = False
+                else:
+                    block_received_crc = checkword ^ offset_word[block_number] # bitwise xor
+                    if block_received_crc == block_calculated_crc:
+                        good_block = True
+                    else:
+                        wrong_blocks_counter += 1
+                        good_block = False
+
+                # Done checking CRC
+                if block_number == 0 and good_block:
+                    group_assembly_started = True
+                    group_good_blocks_counter = 1
+                    bytes = bytearray(8) # 8 bytes filled with 0s
+                if group_assembly_started:
+                    if not good_block:
+                        group_assembly_started = False
+                    else:
+                        # raw data bytes, as received from RDS. 8 info bytes, followed by 4 RDS offset chars: ABCD/ABcD/EEEE (in US) which we leave out here
+                        # RDS information words
+                        # block_number is either 0,1,2,3 so this is how we fill out the 8 bytes
+                        bytes[block_number*2] = (dataword >> 8) & 255
+                        bytes[block_number*2+1] = dataword & 255
+                        group_good_blocks_counter += 1
+                        #print('group_good_blocks_counter:', group_good_blocks_counter)
+                    if group_good_blocks_counter == 5:
+                        #print(bytes)
+                        bytes_out.append(bytes) # list of len-8 lists of bytes
+                block_bit_counter = 0
+                block_number = (block_number + 1) % 4
+                blocks_counter += 1
+                if blocks_counter == 50:
+                    if wrong_blocks_counter > 35: # This many wrong blocks must mean we lost sync
+                        print("Lost Sync (Got ", wrong_blocks_counter, " bad blocks on ", blocks_counter, " total)")
+                        synced = False
+                        presync = False
+                    else:
+                        print("Still Sync-ed (Got ", wrong_blocks_counter, " bad blocks on ", blocks_counter, " total)")
+                    blocks_counter = 0
+                    wrong_blocks_counter = 0
 
 if __name__ == "__main__":
     sdr = RtlSdr()
@@ -56,8 +248,7 @@ if __name__ == "__main__":
     sdr.freq_correction = 60  # PPM
     sdr.bandwidth = 50000
     sdr.gain = 'auto'
-    live_plot()
-    #play_audio_sample()
+    listen_fm()
 
     
 
